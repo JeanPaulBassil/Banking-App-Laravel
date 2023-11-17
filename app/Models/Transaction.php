fadf4f583bf923cfac9666f733c789765df6f9cd
JayPee <jean.paul.bassil@outlook.com>
2023-11-17 15:39:48 +0200

    Added the models

---
+++ app/Models/Transaction.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'account_id',
+        'type',
+        'amount',
+        'currency',
+        'transaction_date',
+    ];
+
+    /**
+     * Get the account associated with the transaction.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function account()
+    {
+        return $this->belongsTo(Account::class);
+    }
+}
