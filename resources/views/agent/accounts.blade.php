e782f4b2c9a334cbd4bc819bdb63ecb69a86332e
Peter Chahid <peterdaaboul@outlook.com>
2023-11-20 04:18:03 +0200

    added styling for better UI, in the agent's views

--- resources/views/agent/accounts.blade.php
+++ resources/views/agent/accounts.blade.php
@@ -5,17 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clients and Their Accounts</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container mt-5">
-        <a href="{{ route('agent.dashboard') }}" class="btn btn-secondary mb-3">Back to Dashboard</a>
+        <a href="{{ route('agent.dashboard') }}" class="btn btn-primary mb-3">Back to Dashboard</a>
         <h2 class="mb-4">Clients and Their Accounts</h2>
         @forelse ($clients as $client)
             <div class="card mb-3">
-                <div class="card-header">{{ $client->name }}</div>
+                <div class="card-header">
+                    Client ID: {{ $client->id }} - {{ $client->name }}
+                </div>
                 <ul class="list-group list-group-flush">
                     @forelse ($client->accounts as $account)
-                        <li class="list-group-item">{{ $account->account_number }} (Balance: {{ $account->balance }})</li>
+                        <li class="list-group-item">
+                            Account: {{ $account->account_number }} (Balance: {{ $account->balance }})
+                        </li>
                     @empty
                         <li class="list-group-item">No accounts available for this client.</li>
                     @endforelse
--- resources/views/agent/operations.blade.php
+++ resources/views/agent/operations.blade.php
@@ -5,19 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Client Operations</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .operation-item {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 10px;
+            border: 1px solid #ddd;
+            margin-bottom: 10px;
+            border-radius: 5px;
+        }
+        .operation-item h5 {
+            margin-bottom: 0;
+        }
+        .operation-item small {
+            color: #666;
+        }
+    </style>
 </head>
 <body>
     <div class="container mt-5">
-        <a href="{{ route('agent.dashboard') }}" class="btn btn-secondary mb-3">Back to Dashboard</a>
+        <a href="{{ route('agent.dashboard') }}" class="btn btn-primary mb-3">Back to Dashboard</a>
         <h2 class="mb-4">Client Operations</h2>
-        <div class="list-group">
+        <div>
             @forelse ($operations as $operation)
-                <a class="list-group-item list-group-item-action">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1">{{ $operation->operation_type }}</h5>
-                        <small>Details: {{ $operation->operation_details }}</small>
-                    </div>
-                </a>
+                <div class="operation-item">
+                    <h5>{{ $operation->operation_type }}</h5>
+                    <small>Details: {{ $operation->operation_details }}</small>
+                </div>
             @empty
                 <p>No operations available.</p>
             @endforelse
